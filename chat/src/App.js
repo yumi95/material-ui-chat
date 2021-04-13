67bb6ebdc733da39c18e586c52f9f42f850b03b9
_yumi_ <yuhey0411@naver.com>
2021-04-13 22:35:38 +0900

    Initial commit

---
+++ chat/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Dashboard from './Dashboard';
+import Store from './Store';
+
+function App() {
+  return (
+    <div className="App">
+      <Store>
+      <Dashboard />
+      </Store>
+    </div>
+  );
+}
+
+export default App;
